31c6d3184f4bbda402fe3af5351b7795eefeb55a
chinna samudrudu <c.samudrudu333@gmail.com>
2018-11-18 12:14:55 +0530

    minor change

--- financialassistant-fe/src/Login.js
+++ financialassistant-fe/src/Login.js
@@ -1,24 +1,57 @@
 import React, {Component} from 'react';
 import classes from './Login.css'
 
+const pagestyles = {
+	display: 'flex',
+	flexDirection: 'row',
+	height:'100%',
+};
+
+const divstylesleft={
+	height:'100%',
+	width:'50%',
+	position:'fixed',
+	zIndex:'1',
+	top:'0',
+	overflowX:'hidden',
+	paddingTop:'20px',
+	left:'0',
+	backgroundColor:'#111',
+}
+const divstylesright={
+	height:'100%',
+	width:'50%',
+	position:'fixed',
+	zIndex:'1',
+	top:'0',
+	overflowX:'hidden',
+	paddingTop:'20px',
+	right:'0',
+	backgroundColor:'gray',
+}
+
 class Login extends Component{
 	render(){
 		return(
-			<div className={`${classes.logindiv}`}>
-				<div>
+			<div>
+				<div  style={divstylesleft}>
 					<img src="" alt="image"/>
 				</div>
-				<div  width="50%">
-					<div className="pa4 black-80">
-						<label htmlFor="username">username</label>
-						<input type="text"/>
-					</div>
-					<div>
-						<label htmlFor="password">password</label>
-						<input type="password"/>
-					</div>
-					<button>Login</button>
-					<a href="">Register</a>
+				<div  style={divstylesright}>
+					<center>
+						<h1>Financial Assistant</h1>
+						<h2>Login</h2>
+						<div>
+							<label htmlFor="username">username</label>
+							<input type="text"/>
+						</div>
+						<div>
+							<label htmlFor="password">password</label>
+							<input type="password"/>
+						</div>
+						<button>Login</button>
+						<a href="">Register</a>
+					</center>
 				</div>
 			</div>
 		)
---
+++ financialassistant-fe/src/Register.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class RegisterForm extends Component{
+	render(){
+		<div>
+			<div>
+				<img src="" height="auto" width="50%" alt="image"/>
+			</div>
+			<div>
+				
+			</div>
+		</div>
+	}
+}
